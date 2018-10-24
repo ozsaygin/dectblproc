9fcd11ac5342b761eda84eb47ee6a8494092a45c
Oguz Ozsaygin <oguzozsaygin@sabanciuniv.edu>
2018-10-24 12:23:29 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+setup(name="dectblproc",
+    version="0.1",
+    description="A decision-logic-table processor",
+    url="http://github.com/ozsaygin/dectblproc",
+    author="Oguz Ozsaygin",
+    author_email="oguzozsaygin@sabanciuniv.edu",
+)
